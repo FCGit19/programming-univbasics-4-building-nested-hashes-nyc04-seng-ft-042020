d99c880dc3f9fde9695b0862f781fa3b2fbb4ca6
Fred C <canofred@gmail.com>
2020-03-29 03:18:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,9 +4,9 @@ end
 
 def monopoly_with_second_tier
   if {:railroads => {}}
-   hash_object[:pieces] = 4
-   hash_object[:rent_in_dollars] = {}
-   hash_object[:names] = {}
+   hash_object[:pieces] => 4
+   hash_object[:rent_in_dollars] => {}
+   hash_object[:names] => {}
  end 
 end
 
