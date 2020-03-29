95359c6b95a750dc5413a6948464d1e27304e8f3
Fred C <canofred@gmail.com>
2020-03-29 03:26:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ def monopoly_with_second_tier
    {:railroads => {}}[:pieces] = 4
    {:railroads => {}}[:rent_in_dollars] = {}
    {:railroads => {}}[:names] = {}
- end 
+  end 
 end
 
 def monopoly_with_third_tier
